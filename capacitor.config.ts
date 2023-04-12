e4432b5b0681edac1577ca8dda0508f6c1247033
francocores <francocores@gmail.com>
2023-04-12 18:46:13 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.multi.task.app',
+  appName: 'multiTaskApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
