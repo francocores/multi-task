9ae71990bd75cf0d4217ece2d581aa14d85e109d
francocores <francocores@gmail.com>
2023-04-13 20:49:58 +0200

    navigation singleton service

---
+++ src/app/core/nav.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NavService extends NavController { }
