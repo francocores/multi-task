8d142612bfd525565a2e3a2b0109e4b2a1adde1a
francocores <francocores@gmail.com>
2023-04-14 09:11:17 +0200

    set mode ios and ES locale for angular

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { LOCALE_ID, enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { registerLocaleData } from '@angular/common';
+import es from '@angular/common/locales/es';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+
+registerLocaleData(es);
 
 if (environment.production) {
   enableProdMode();
@@ -14,7 +18,8 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    importProvidersFrom(IonicModule.forRoot({ mode: 'md' })),
+    { provide: LOCALE_ID, useValue: 'es-ES' },
+    importProvidersFrom(IonicModule.forRoot({ mode: 'ios' })),
     provideRouter(routes),
   ],
 });
